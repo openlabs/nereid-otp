9ee0455772aadf34b3c63e62f179fd4c6d6b8b6e
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2012-09-10 12:50:48 +0530

    Add an __init__.py to start the project

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    __init__
+
+    Add OATH based Two factor authentication to Login
+
+    :copyright: (c) 2012 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
+
